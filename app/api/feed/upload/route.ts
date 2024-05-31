60fe908edd2299f76e80c301b5a3ed8a42b84b32
Mike E <mike@greenphoenixengineering.com>
2024-05-31 12:01:00 -0500

    adds debugger file, email integration stub

--- app/api/feed/upload/route.ts
+++ app/api/feed/upload/route.ts
@@ -9,6 +9,7 @@ type RequestBody = {
 export async function POST(request: Request) {
   const { transactions }: RequestBody = await request.json();
   try {
+    // upsert newly uploaded sales transactions
     let data = { uploaded: 0, updated: 0, matched: 0 };
 
     let allPropertyIds: string[] = transactions.map((t) => t.PropertyID);
@@ -23,28 +24,59 @@ export async function POST(request: Request) {
 
     await supabaseAdmin.from("sales_transaction").upsert(transactions);
 
+    // get search for matching referrals
     let referralsResp = await supabaseAdmin.from("referral").select("*");
 
     if (referralsResp.data?.length) {
       for (let i = 0; i < referralsResp.data.length; i++) {
-        const r = referralsResp.data[i];
+        const referral = referralsResp.data[i];
 
         let matches = transactions.filter(
-          (t) => t.Grantee?.toLowerCase() === r?.track_name?.toLowerCase()
+          (t) => t.Grantee?.toLowerCase() === referral?.track_name?.toLowerCase()
         );
 
+        // on found, store the match and submit email
         if (matches.length) {
           data.matched += matches.length;
 
           let matchesData = matches.map((m) => ({
             sold_property_id: m?.PropertyID,
-            referral_id: r.id,
+            referral_id: referral.id,
           }));
 
           await supabaseAdmin.from("matches").upsert(matchesData);
+
+          console.log('submitting email...');
+
+          debugger;
+
+          const response = await fetch('/workspaces/fd65229c-ce57-4787-9fe6-f8fc1bf18d63/channels/d07adb03-1cd1-495f-b963-b18505a6b92c/messages', {
+            method: 'POST',
+            headers: {
+              "Authorization": "AccessKey mKzn3uh3uSZnPQeZGp71ZWvIvnxsw8cWMqTP",
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              "receiver": {
+                "contacts": [
+                  {
+                    "identifierValue": referral?.email?.toLowerCase() // agent's emal
+                  }
+                ]
+              },
+              "body": `Congratulations! Your referral ${referral?.track_name?.toLowerCase()} has purchased a property. 
+              Now is the time to reach out for your compensation!`
+            }),
+        });
+        
+        await response.json();
+        debugger;
+        
         }
       }
     }
+    
+    debugger;
 
     return NextResponse.json(
       {
@@ -60,3 +92,5 @@ export async function POST(request: Request) {
     });
   }
 }
+
+
